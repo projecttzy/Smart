b3635c5eb28ca5a6b4092337472c4d8913cd9bd3
morishitamakoto0330 <morishita0330@gmail.com>
2019-04-30 19:43:24 +0900

    first commit

---
+++ Smart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart
+//
+//  Created by MakotoMorishita on 2019/04/30.
+//  Copyright © 2019 TZY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
